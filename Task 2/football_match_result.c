f2a56781932528f954e6dd785ac6279ee3d6a2b7
isaac <isaactbb05@gmail.com>
2025-12-04 01:09:40 +0100

    First Version of football_match_result

---
+++ Task 2/football_match_result.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() {
+    int home_team_goals , away_team_goals;
+
+    // Asking the user for the result of the match
+    printf("Enter the Goals from Home Team: ");
+    scanf("%d",&home_team_goals);
+    printf("Enter the Goals from Away Team: ");
+    scanf("%d",&away_team_goals);
+
+    //Logical Conditions
+    if (home_team_goals>away_team_goals) {
+        printf("The home team has won");
+    }
+    else if (home_team_goals<away_team_goals) {
+        printf("The home team has lost");
+    }
+    else {
+        printf("It is a draw");
+    }
+
+    return 0;
+}
